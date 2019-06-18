953136a91a815e52b61bbf58b242371e3da12f21
jwting <45863361+jwting@users.noreply.github.com>
2019-06-18 19:34:09 +0800

    Add files via upload

--- 后台管理系统/js/main.js
+++ 后台管理系统/js/main.js
@@ -20,48 +20,162 @@ $(function () {
             iconChevron.addClass('iconRotate');
         }
     }
+
+    layui.use(['element', 'table', 'laydate', 'laypage', 'layer', 'upload'], function () {
+        var laypage = layui.laypage; //分页操作
+        var laydate = layui.laydate; //日期操作
+        var layer = layui.layer; //弹出框操作
+        var table = layui.table; //表格操作
+        var upload = layui.upload; //文件上传操作
+
+        // Tab栏切换功能
+        var element = layui.element; //Tab的切换功能，切换事件监听等，需要依赖element模块
+        //触发事件
+        var active = {
+            tabAdd: function () {
+                //新增一个Tab项
+                element.tabAdd('demo', {
+                    title: '新选项' + (Math.random() * 1000 | 0) //用于演示
+                        ,
+                    content: '内容' + (Math.random() * 1000 | 0),
+                    id: new Date().getTime() //实际使用一般是规定好的id，这里以时间戳模拟下
+                })
+            },
+        };
+        // $('.site-demo-active').on('click', function () {
+        //     var othis = $(this),
+        //         type = othis.data('type');
+        //     active[type] ? active[type].call(this, othis) : '';
+        // });
+
+        //监听表格复选框选择
+        table.on('checkbox(demo)', function (obj) {
+            console.log(obj)
+        });
+        //监听工具条
+        table.on('tool(demo)', function (obj) {
+            var data = obj.data;
+            // if (obj.event === 'detail') {
+            //     layer.msg('ID：' + data.id + ' 的查看操作');
+            // } else if (obj.event === 'del') {
+            //     layer.confirm('真的删除行么', function (index) {
+            //         obj.del();
+            //         layer.close(index);
+            //     });
+            // } else if (obj.event === 'del') {
+            //     layer.alert('编辑行：<br>' + JSON.stringify(data))
+            // }
+            if (obj.event === 'edit') {
+                layer.open({
+                    type: 2,
+                    skin: 'layui-layer-rim', //加上边框
+                    area: ['900px', '800px'], //宽高
+                    content: 'popup-merge.html',
+                    title:'族谱合并',
+                    btn: ['取消', '确定']
+                });
+            }else if (obj.event === 'same') {
+                layer.open({
+                    type: 2,
+                    skin: 'layui-layer-rim', //加上边框
+                    area: ['900px', '800px'], //宽高
+                    content: 'same-merge.html',
+                    title:'族谱合并',
+                    btn: ['取消', '确定']
+                });
+            }
+        });
+
+        var $ = layui.$,
+            active = {
+                getCheckData: function () { //获取选中数据
+                    var checkStatus = table.checkStatus('idTest'),
+                        data = checkStatus.data;
+                    layer.alert(JSON.stringify(data));
+                },
+                getCheckLength: function () { //获取选中数目
+                    var checkStatus = table.checkStatus('idTest'),
+                        data = checkStatus.data;
+                    layer.msg('选中了：' + data.length + ' 个');
+                },
+                isAll: function () { //验证是否全选
+                    var checkStatus = table.checkStatus('idTest');
+                    layer.msg(checkStatus.isAll ? '全选' : '未全选')
+                }
+            };
+
+        $('.demoTable .layui-btn').on('click', function () {
+            var type = $(this).data('type');
+            active[type] ? active[type].call(this) : '';
+        });
+
+        // 分页
+        laypage.render({
+            elem: 'demo7', //分页容器的id
+            count: 1000, //总条数
+            layout: ['count', 'prev', 'page', 'next', 'limit', 'skip'],
+            //,skip: true //开启跳页
+            jump: function (obj, first) {
+                if (!first) {
+                    layer.msg('第' + obj.curr + '页', {
+                        offset: 'b'
+                    });
+                }
+            }
+        });
+        // 分页
+        laypage.render({
+            elem: 'demo8', //分页容器的id
+            count: 1000, //总条数
+            layout: ['count', 'prev', 'page', 'next', 'limit', 'skip'],
+            //,skip: true //开启跳页
+            jump: function (obj, first) {
+                if (!first) {
+                    layer.msg('第' + obj.curr + '页', {
+                        offset: 'b'
+                    });
+                }
+            }
+        });
+
+        //日期范围
+        laydate.render({
+            elem: '#test6',
+            range: true
+        });
+        //日期时间选择器
+        laydate.render({
+            elem: '#test5',
+            type: 'datetime'
+        });
+
+        //普通图片上传
+        var uploadInst = upload.render({
+            elem: '#test1',
+            url: '/upload/',
+            before: function (obj) {
+                //预读本地文件示例，不支持ie8
+                obj.preview(function (index, file, result) {
+                    $('#demo1').attr('src', result); //图片链接（base64）
+                });
+            },
+            done: function (res) {
+                //如果上传失败
+                if (res.code > 0) {
+                    return layer.msg('上传失败');
+                }
+                //上传成功
+            },
+            error: function () {
+                //演示失败状态，并实现重传
+                var demoText = $('#demoText');
+                demoText.html('<span style="color: #FF5722;">上传失败</span> <a class="layui-btn layui-btn-xs demo-reload">重试</a>');
+                demoText.find('.demo-reload').on('click', function () {
+                    uploadInst.upload();
+                });
+            }
+        });
+    });
+
+
 })
-
-layui.use('element', function () {
-    var $ = layui.jquery,
-        element = layui.element; //Tab的切换功能，切换事件监听等，需要依赖element模块
-
-    //触发事件
-    var active = {
-        tabAdd: function () {
-            //新增一个Tab项
-            element.tabAdd('demo', {
-                title: '新选项' + (Math.random() * 1000 | 0) //用于演示
-                    ,
-                content: '内容' + (Math.random() * 1000 | 0),
-                id: new Date().getTime() //实际使用一般是规定好的id，这里以时间戳模拟下
-            })
-        },
-        tabDelete: function (othis) {
-            //删除指定Tab项
-            element.tabDelete('demo', '44'); //删除：“商品管理”
-
-
-            othis.addClass('layui-btn-disabled');
-        },
-        tabChange: function () {
-            //切换到指定Tab项
-            element.tabChange('demo', '22'); //切换到：用户管理
-        }
-    };
-
-    $('.site-demo-active').on('click', function () {
-        var othis = $(this),
-            type = othis.data('type');
-        active[type] ? active[type].call(this, othis) : '';
-    });
-
-    //Hash地址的定位
-    var layid = location.hash.replace(/^#test=/, '');
-    element.tabChange('test', layid);
-
-    element.on('tab(test)', function (elem) {
-        location.hash = 'test=' + $(this).attr('lay-id');
-    });
-
-});
